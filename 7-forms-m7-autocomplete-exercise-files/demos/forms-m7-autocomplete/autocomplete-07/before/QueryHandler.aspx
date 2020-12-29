5ed11ef60e264c6505f540a7cc898c484f152b7a
Alex Fuller <fulleralex47@gmail.com>
2020-12-29 08:45:03 -0500

    Add a remote typeahead lookup and a fake local remote server.

---
+++ 7-forms-m7-autocomplete-exercise-files/demos/forms-m7-autocomplete/autocomplete-07/before/QueryHandler.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="QueryHandler.aspx.cs" Inherits="forms_m7_autocomplete.autocomplete_07.before.QueryHandler1" %>
+
+
---
+++ 7-forms-m7-autocomplete-exercise-files/demos/forms-m7-autocomplete/autocomplete-07/before/QueryHandler.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace forms_m7_autocomplete.autocomplete_07.before
+{
+    public partial class QueryHandler1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var teams = new List<string>()
+            {
+                "Giants",
+                "Jets"
+            };
+
+            var q = Request.QueryString["q"];
+            var result = teams.Where(t => t.ToUpper().StartsWith(q.ToUpper()));
+
+            var jss = new JavaScriptSerializer();
+            var output = jss.Serialize(result);
+            Response.Write(output);
+        }
+    }
+}
